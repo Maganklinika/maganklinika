4a61d877403cfd33239d4f96c061ff4524c5546f
BelaBordacs <side1111@hotmail.com>
2025-02-15 13:56:01 +0100

    specifiáció és backend

--- maganklinika/backend/database/migrations/2025_02_08_090846_create_doctor_appointments_table.php
+++ maganklinika/backend/database/migrations/2025_02_08_090846_create_doctor_appointments_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->datetime('start_time'); //key
             $table->foreignId('user_id')->references('user_id')->on('patients');
             $table->foreignId('treatment_id')->references('treatment_id')->on('treatments');
-            $table->enum('status', ['booked', 'done', 'deleted_by_doctor', 'cancelled_by_patient']);
+            $table->enum('status', ['vacant', 'booked', 'done', 'deleted_by_doctor', 'cancelled_by_patient']);
             $table->integer('rating');
             $table->timestamps();
             $table->primary(['doctor_id', 'start_time']);
