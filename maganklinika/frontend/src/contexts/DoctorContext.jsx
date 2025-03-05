3b90451d084d28e587356f6402d460098bae5563
kares <karesz333@hotmail.com>
2025-03-05 20:11:59 +0100

    időpont létrehozás javítzása

--- maganklinika/frontend/src/contexts/DoctorContext.jsx
+++ maganklinika/frontend/src/contexts/DoctorContext.jsx
@@ -3,49 +3,49 @@ import { myAxios } from "../api/Axios";
 
 const DoctorContext = createContext();
 
-export const DoctorProvider = ({ children }) => {
-  const [doctorsWithSpec, setDoctorsWithSpec] = useState([]);
-  const [filteredDoctorsList, setFilteredDoctorsList] = useState([]);
-  const [appointmentsByDoctor, setAppointmentsByDoctor] = useState([]);
-  const [appointmentsByDate, setAppointmentsByDate] = useState({});
-  const [allPatients, setAllPatients] = useState([]);
-  const [filteredAllPatientsList, setFilteredAllPatientsList] = useState([]);
-  const [patientsByAppointments, setPatientsByAppointments] = useState([]);
+export const DoctorProvider = ( { children } ) => {
+  const [ doctorsWithSpec, setDoctorsWithSpec ] = useState( [] );
+  const [ filteredDoctorsList, setFilteredDoctorsList ] = useState( [] );
+  const [ appointmentsByDoctor, setAppointmentsByDoctor ] = useState( [] );
+  const [ appointmentsByDate, setAppointmentsByDate ] = useState( {} );
+  const [ allPatients, setAllPatients ] = useState( [] );
+  const [ filteredAllPatientsList, setFilteredAllPatientsList ] = useState( [] );
+  const [ patientsByAppointments, setPatientsByAppointments ] = useState( [] );
   const [
     filteredPatientsByAppointmentsList,
     setFilteredPatientsByAppointmentsList,
-  ] = useState([]);
-  const [appointmentsByPatients, setAppointmentsByPatients] = useState([]);
-  const [appointmentCount, setAppointmentCount] = useState([]);
+  ] = useState( [] );
+  const [ appointmentsByPatients, setAppointmentsByPatients ] = useState( [] );
+  const [ appointmentCount, setAppointmentCount ] = useState( [] );
 
   const fetchAllPatients = async () => {
-    const response = await myAxios.get("/api/get-all-patients-with-name");
-    setAllPatients(response.data);
-    setFilteredAllPatientsList([...response.data]);
+    const response = await myAxios.get( "/api/get-all-patients-with-name" );
+    setAllPatients( response.data );
+    setFilteredAllPatientsList( [ ...response.data ] );
   };
 
-  const fetchAppointmentByPatients = async (pac_id) => {
+  const fetchAppointmentByPatients = async ( pac_id ) => {
     const response = await myAxios.get(
-      `/api/get-appointments-by-patients/${pac_id}`
+      `/api/get-appointments-by-patients/${ pac_id }`
     );
-    setAppointmentsByPatients(response.data);
+    setAppointmentsByPatients( response.data );
   };
 
   const fetchAppointmentsCount = async () => {
-    const response = await myAxios.get(`/api/get-appointments-count`);
-    setAppointmentCount(response.data);
+    const response = await myAxios.get( `/api/get-appointments-count` );
+    setAppointmentCount( response.data );
   };
 
   const fetchPatientsByAppointments = async () => {
-    const response = await myAxios.get("/api/get-patients-to-auth-doctor");
-    setPatientsByAppointments(response.data);
-    setFilteredPatientsByAppointmentsList([...response.data]);
+    const response = await myAxios.get( "/api/get-patients-to-auth-doctor" );
+    setPatientsByAppointments( response.data );
+    setFilteredPatientsByAppointmentsList( [ ...response.data ] );
   };
 
   const fetchDoctorsWithSpec = async () => {
-    const response = await myAxios.get("/api/doctors-with-spec");
-    setDoctorsWithSpec(response.data);
-    setFilteredDoctorsList([...response.data]);
+    const response = await myAxios.get( "/api/doctors-with-spec" );
+    setDoctorsWithSpec( response.data );
+    setFilteredDoctorsList( [ ...response.data ] );
   };
 
   const fetchDoctorData = async () => {
@@ -55,25 +55,30 @@ export const DoctorProvider = ({ children }) => {
     await fetchAppointmentsCount();
   };
 
-  /*const fetchAppontmentsByDoctor = async () => {
-    const response = await myAxios.get("/api/get-appointments-by-doctor");
-    setAppointmentsByDoctor(response.data);
-    groupAppointmentsByDate(response.data);
-  };*/
-
-  const groupAppointmentsByDate = (appointmentsArray) => {
-    // Csoportosítjuk az appointments-t a start_time alapján dátum szerint
-    const groupedAppointments = appointmentsArray.reduce((acc, appointment) => {
-      // Csak a dátumot veszük figyelembe (YYYY-MM-DD)
-      const date = appointment.start_time.split(" ")[0];
-      if (!acc[date]) {
-        acc[date] = [];
+  const fetchAppontmentsByDoctor = async ( doctor_id ) => {
+    console.log( doctor_id )
+    const response = await myAxios.get( "/api/get-appointments-by-doctor", {
+      params: {
+        doctor_id: doctor_id, // A doctor_id értéke, amit át akarsz adni
       }
-      acc[date].push(appointment);
-      return acc;
-    }, {});
+    } );
+    setAppointmentsByDoctor( response.data );
+    groupAppointmentsByDate( response.data );
+  };
 
-    setAppointmentsByDate(groupedAppointments);
+  const groupAppointmentsByDate = ( appointmentsArray ) => {
+    // Csoportosítjuk az appointments-t a start_time alapján dátum szerint
+    const groupedAppointments = appointmentsArray.reduce( ( acc, appointment ) => {
+      // Csak a dátumot veszük figyelembe (YYYY-MM-DD)
+      const date = appointment.start_time.split( " " )[ 0 ];
+      if ( !acc[ date ] ) {
+        acc[ date ] = [];
+      }
+      acc[ date ].push( appointment );
+      return acc;
+    }, {} );
+
+    setAppointmentsByDate( groupedAppointments );
   };
 
   return (
@@ -85,7 +90,7 @@ export const DoctorProvider = ({ children }) => {
         filteredDoctorsList,
         appointmentsByDoctor,
         appointmentsByDate,
-        //fetchAppontmentsByDoctor,
+        fetchAppontmentsByDoctor,
         fetchAllPatients,
         allPatients,
         filteredAllPatientsList,
@@ -104,5 +109,5 @@ export const DoctorProvider = ({ children }) => {
 };
 
 export default function useDoctorContext() {
-  return useContext(DoctorContext);
+  return useContext( DoctorContext );
 }
