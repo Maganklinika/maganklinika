4e6b288caa615a85723774015609b9237f507745
kares <karesz333@hotmail.com>
2025-04-25 13:11:54 +0200

    spec validáció helyes megjelenítésének javítása

--- maganklinika/frontend/src/pages/Regisztracio.jsx
+++ maganklinika/frontend/src/pages/Regisztracio.jsx
@@ -5,28 +5,28 @@ import './regisztracio.css';
 
 
 const Regisztracio = () => {
-  const [selectedValue, setSelectedValue] = useState("patient");
-  const [name, setName] = useState("");
-  const [birthDay, setBirthDay] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [phone, setPhone] = useState("");
-  const [taj, setTaj] = useState("");
-  const [licence, setLicence] = useState("");
-  const [address, setAddress] = useState("");
-  const [password_confirmation, setPassword_confirmation] = useState("");
-  const [spec_id, setSpec] = useState("");
-  const [localErrors, setLocalErrors] = useState({});
+  const [ selectedValue, setSelectedValue ] = useState( "patient" );
+  const [ name, setName ] = useState( "" );
+  const [ birthDay, setBirthDay ] = useState( "" );
+  const [ email, setEmail ] = useState( "" );
+  const [ password, setPassword ] = useState( "" );
+  const [ phone, setPhone ] = useState( "" );
+  const [ taj, setTaj ] = useState( "" );
+  const [ licence, setLicence ] = useState( "" );
+  const [ address, setAddress ] = useState( "" );
+  const [ password_confirmation, setPassword_confirmation ] = useState( "" );
+  const [ spec_id, setSpec ] = useState( "" );
+  const [ localErrors, setLocalErrors ] = useState( {} );
 
   const { reg, errors, fetchSpecializations, specializations, checkDoctorLicence, isValidLicence } = useAuthContext();
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async ( e ) => {
     e.preventDefault();
 
     let licenceResponse = "";
 
-    if (selectedValue === "doctor") {
-      licenceResponse = await checkDoctorLicence(licence);
+    if ( selectedValue === "doctor" ) {
+      licenceResponse = await checkDoctorLicence( licence );
     }
 
     const adat = {
@@ -43,35 +43,35 @@ const Regisztracio = () => {
       specialization_id: spec_id,
     }
 
-    if (selectedValue === "doctor") {
-      if (licenceResponse.statusText === "OK") {
-        reg(adat);
+    if ( selectedValue === "doctor" ) {
+      if ( licenceResponse.statusText === "OK" ) {
+        reg( adat );
       } else {
-        setLocalErrors((prev) => ({
+        setLocalErrors( ( prev ) => ( {
           ...prev,
           licence: licenceResponse.message,
-        }));
+        } ) );
       }
-    } else if (selectedValue === "patient") {
-      reg(adat);
+    } else if ( selectedValue === "patient" ) {
+      reg( adat );
     }
 
   };
 
-  const radiobuttonClickHandle = (e) => {
-    setName("");
-    setBirthDay("");
-    setAddress("");
-    setEmail("");
-    setLicence("");
-    setPassword("");
-    setPassword_confirmation("");
-    setTaj("");
-    setPhone("");
-    setSelectedValue(e);
-    if (e === "doctor")
+  const radiobuttonClickHandle = ( e ) => {
+    setName( "" );
+    setBirthDay( "" );
+    setAddress( "" );
+    setEmail( "" );
+    setLicence( "" );
+    setPassword( "" );
+    setPassword_confirmation( "" );
+    setTaj( "" );
+    setPhone( "" );
+    setSelectedValue( e );
+    if ( e === "doctor" )
       fetchSpecializations();
-    setSpec("");
+    setSpec( "" );
   }
 
 
@@ -88,7 +88,7 @@ const Regisztracio = () => {
             id="patient"
             value="patient"
             checked={selectedValue === "patient"}
-            onChange={(e) => radiobuttonClickHandle(e.target.value)}
+            onChange={( e ) => radiobuttonClickHandle( e.target.value )}
           />
           <Form.Check
             inline
@@ -98,7 +98,7 @@ const Regisztracio = () => {
             id="doctor"
             value="doctor"
             checked={selectedValue === "doctor"}
-            onChange={(e) => radiobuttonClickHandle(e.target.value)}
+            onChange={( e ) => radiobuttonClickHandle( e.target.value )}
           />
         </Form.Group>
         {selectedValue === "patient" ? (
@@ -113,14 +113,14 @@ const Regisztracio = () => {
                     <input
                       type="text"
                       value={name}
-                      onChange={(e) => setName(e.target.value)}
+                      onChange={( e ) => setName( e.target.value )}
                       className="form-control"
                       id="name"
                       placeholder="név"
                       name="name"
                     />
                   </div>
-                  <div>{errors.name && <span className="text-danger">{errors.name[0]}</span>}</div>
+                  <div>{errors.name && <span className="text-danger">{errors.name[ 0 ]}</span>}</div>
 
                   <div className="mb-3 mt-3">
                     <label htmlFor="phone" className="form-label">
@@ -129,14 +129,14 @@ const Regisztracio = () => {
                     <input
                       type="text"
                       value={phone}
-                      onChange={(e) => setPhone(e.target.value)}
+                      onChange={( e ) => setPhone( e.target.value )}
                       className="form-control"
                       id="phone"
                       placeholder="Telefonszám"
                       name="phone"
                     />
                   </div>
-                  <div>{errors.phone_number && <span className="text-danger">{errors.phone_number[0]}</span>}</div>
+                  <div>{errors.phone_number && <span className="text-danger">{errors.phone_number[ 0 ]}</span>}</div>
 
                   <div className="mb-3 mt-3">
                     <label htmlFor="email" className="form-label">
@@ -145,14 +145,14 @@ const Regisztracio = () => {
                     <input
                       type="email"
                       value={email}
-                      onChange={(e) => setEmail(e.target.value)}
+                      onChange={( e ) => setEmail( e.target.value )}
                       className="form-control"
                       id="email"
                       placeholder="email"
                       name="email"
                     />
                   </div>
-                  <div>{errors.email && <span className="text-danger">{errors.email[0]}</span>}</div>
+                  <div>{errors.email && <span className="text-danger">{errors.email[ 0 ]}</span>}</div>
 
                   <div className="mb-3 mt-3">
                     <label htmlFor="birthDay" className="form-label">
@@ -161,13 +161,13 @@ const Regisztracio = () => {
                     <input
                       type="date"
                       value={birthDay}
-                      onChange={(e) => setBirthDay(e.target.value)}
+                      onChange={( e ) => setBirthDay( e.target.value )}
                       className="form-control"
                       id="birthDay"
                       name="birthDay"
                     />
                   </div>
-                  <div>{errors.birth_date && <span className="text-danger">{errors.birth_date[0]}</span>}</div>
+                  <div>{errors.birth_date && <span className="text-danger">{errors.birth_date[ 0 ]}</span>}</div>
                 </Form.Group>
               </Col>
 
@@ -180,14 +180,14 @@ const Regisztracio = () => {
                     <input
                       type="text"
                       value={address}
-                      onChange={(e) => setAddress(e.target.value)}
+                      onChange={( e ) => setAddress( e.target.value )}
                       className="form-control"
                       id="address"
                       placeholder="Cím"
                       name="address"
                     />
                   </div>
-                  <div>{errors.address && <span className="text-danger">{errors.address[0]}</span>}</div>
+                  <div>{errors.address && <span className="text-danger">{errors.address[ 0 ]}</span>}</div>
 
                   <div className="mb-3 mt-3">
                     <label htmlFor="taj" className="form-label">
@@ -196,14 +196,14 @@ const Regisztracio = () => {
                     <input
                       type="number"
                       value={taj}
-                      onChange={(e) => setTaj(e.target.value)}
+                      onChange={( e ) => setTaj( e.target.value )}
                       className="form-control"
                       id="taj"
                       placeholder="Taj szám"
                       name="taj"
                     />
                   </div>
-                  <div>{errors.taj_number && <span className="text-danger">{errors.taj_number[0]}</span>}</div>
+                  <div>{errors.taj_number && <span className="text-danger">{errors.taj_number[ 0 ]}</span>}</div>
 
                   <div className="mb-3">
                     <label htmlFor="pwd" className="form-label">
@@ -212,13 +212,13 @@ const Regisztracio = () => {
                     <input
                       type="password"
                       value={password}
-                      onChange={(e) => setPassword(e.target.value)}
+                      onChange={( e ) => setPassword( e.target.value )}
                       className="form-control"
                       id="pwd"
                       placeholder="jelszó"
                       name="pwd"
                     />
-                    <div>{errors.password && <span className="text-danger">{errors.password[0]}</span>}</div>
+                    <div>{errors.password && <span className="text-danger">{errors.password[ 0 ]}</span>}</div>
                   </div>
                   <div className="mb-3">
                     <label htmlFor="pwdcmn" className="form-label">
@@ -227,7 +227,7 @@ const Regisztracio = () => {
                     <input
                       type="password"
                       value={password_confirmation}
-                      onChange={(e) => setPassword_confirmation(e.target.value)}
+                      onChange={( e ) => setPassword_confirmation( e.target.value )}
                       className="form-control"
                       id="pwdcmn"
                       placeholder="jelszó újra"
@@ -250,14 +250,14 @@ const Regisztracio = () => {
                     <input
                       type="text"
                       value={name}
-                      onChange={(e) => setName(e.target.value)}
+                      onChange={( e ) => setName( e.target.value )}
                       className="form-control"
                       id="name"
                       placeholder="név"
                       name="name"
                     />
                   </div>
-                  <div>{errors.name && <span className="text-danger">{errors.name[0]}</span>}</div>
+                  <div>{errors.name && <span className="text-danger">{errors.name[ 0 ]}</span>}</div>
 
                   <div className="mb-3 mt-3">
                     <label htmlFor="phone" className="form-label">
@@ -266,14 +266,14 @@ const Regisztracio = () => {
                     <input
                       type="text"
                       value={phone}
-                      onChange={(e) => setPhone(e.target.value)}
+                      onChange={( e ) => setPhone( e.target.value )}
                       className="form-control"
                       id="phone"
                       placeholder="Telefonszám"
                       name="phone"
                     />
                   </div>
-                  <div>{errors.phone && <span className="text-danger">{errors.phone[0]}</span>}</div>
+                  <div>{errors.phone && <span className="text-danger">{errors.phone[ 0 ]}</span>}</div>
 
                   <div className="mb-3 mt-3">
                     <label htmlFor="email" className="form-label">
@@ -282,14 +282,14 @@ const Regisztracio = () => {
                     <input
                       type="email"
                       value={email}
-                      onChange={(e) => setEmail(e.target.value)}
+                      onChange={( e ) => setEmail( e.target.value )}
                       className="form-control"
                       id="email"
                       placeholder="email"
                       name="email"
                     />
                   </div>
-                  <div>{errors.email && <span className="text-danger">{errors.email[0]}</span>}</div>
+                  <div>{errors.email && <span className="text-danger">{errors.email[ 0 ]}</span>}</div>
                 </Form.Group>
               </Col>
 
@@ -302,14 +302,14 @@ const Regisztracio = () => {
                     <input
                       type="number"
                       value={licence}
-                      onChange={(e) => setLicence(e.target.value)}
+                      onChange={( e ) => setLicence( e.target.value )}
                       className="form-control"
                       id="licence"
                       placeholder="Orvosi Licensz szám"
                       name="licence"
                     />
                   </div>
-                  <div>{errors.licence && <span className="text-danger">{errors.licence[0]}</span>}</div>
+                  <div>{errors.licence && <span className="text-danger">{errors.licence[ 0 ]}</span>}</div>
 
                   <div className="mb-3">
                     <label htmlFor="pwd" className="form-label">
@@ -318,13 +318,13 @@ const Regisztracio = () => {
                     <input
                       type="password"
                       value={password}
-                      onChange={(e) => setPassword(e.target.value)}
+                      onChange={( e ) => setPassword( e.target.value )}
                       className="form-control"
                       id="pwd"
                       placeholder="jelszó"
                       name="pwd"
                     />
-                    <div>{errors.password && <span className="text-danger">{errors.password[0]}</span>}</div>
+                    <div>{errors.password && <span className="text-danger">{errors.password[ 0 ]}</span>}</div>
                   </div>
                   <div className="mb-3">
                     <label htmlFor="pwdcmn" className="form-label">
@@ -333,7 +333,7 @@ const Regisztracio = () => {
                     <input
                       type="password"
                       value={password_confirmation}
-                      onChange={(e) => setPassword_confirmation(e.target.value)}
+                      onChange={( e ) => setPassword_confirmation( e.target.value )}
                       className="form-control"
                       id="pwdcmn"
                       placeholder="jelszó újra"
@@ -349,23 +349,23 @@ const Regisztracio = () => {
               </label>
               <Form.Select
                 aria-label="select-spec"
-                onChange={(event) => {
-                  setSpec(Number(event.target.value));
+                onChange={( event ) => {
+                  setSpec( Number( event.target.value ) );
                 }}
               >
                 <option value={"-1"}>Válassz a listából</option>
                 {specializations
-                  ? specializations.map((e) => (
+                  ? specializations.map( ( e ) => (
                     <option key={e.specialization_id} value={e.specialization_id}>
                       {e.specialization_name}
                     </option>
-                  ))
+                  ) )
                   : ""}
               </Form.Select>
             </div>
+            {localErrors.licence && <span className="text-danger">{localErrors.licence}</span>}
           </Container>
         )}
-        {localErrors.licence && <span className="text-danger">{localErrors.licence}</span>}
         <div className="text-center">
           <Button type="submit" className="btn btn-primary w-50">
             Regisztráció
